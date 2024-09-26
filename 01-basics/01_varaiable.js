0c7a2d0574a4078f40ceb6287e798bd5f60ca055
kiran shah <162853046+sahakira@users.noreply.github.com>
2024-09-26 15:20:16 +0000

    pre

---
+++ 01-basics/01_varaiable.js
@@ -0,0 +1,12 @@
+const accountId="72593470"
+let accountName="civil"
+var accountEmail="kiranshah7929@gmail.com"
+accountCity="kathmandu"
+// accountId=2
+// console.log(accountName);
+
+
+accountEmail="aliyanashah927@gmail.com"
+accountName="prabhu"
+accountCity="birgunj"
+console.table([accountEmail,accountName,accountCity]);
---
+++ 01-basics/02_datatype.js
@@ -0,0 +1,6 @@
+let age=18
+let name="kiran"
+let isLoggedIn=true;
+// console.log(typeof "kiran");
+// console.log(typeof undefined);
+console.log(typeof null);
